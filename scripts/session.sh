c9f5565c710e9046e640b981fd5edb162aa0c6ec
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2024-12-14 14:31:41 -0800

    feat: creates new dir for new projects

--- scripts/session.sh
+++ scripts/session.sh
@@ -3,12 +3,23 @@
 
 dir=$(fd . ~/repos -t d -d 2 | sk)
 if [[ -n $dir ]]; then
-  target=$(basename $dir)
-  tmux new-session -d -A -s $target -c $dir
-  tmux switch-client -t $target
-  tmux send-keys -t $target "cd src; nvim -c 'Telescope find_files'" C-m
-  tmux new-window -d -t $target -c $dir
-  if [[ -z $TMUX ]]; then
+  if [[ $(dirname $dir) == $HOME/repos ]]; then
+    echo -n "Name: "
+    read -r target
+    if [[ -n $target ]]; then
+      mkdir -p $dir$target
+      tmux new-session -d -A -s $target -c $dir/$target
+      tmux switch-client -t $target
+      tmux new-window -d -t $target -c $dir/$target
+    fi
+  else
+    target=$(basename $dir)
+    tmux new-session -d -A -s $target -c $dir
+    tmux switch-client -t $target
+    tmux send-keys -t $target 'cd src; nvim -c "Telescope find_files"' C-m
+    tmux new-window -d -t $target -c $dir
+  fi
+  if [[ -n $target && -z $TMUX ]]; then
     tmux attach -t $target
   fi
 fi
