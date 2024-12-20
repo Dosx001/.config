2280c25e46b5964e6cab046d5b9091aa62181029
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2024-12-19 16:56:38 -0800

    feat: create or clone new projects for sessions

--- scripts/session.sh
+++ scripts/session.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 # shellcheck disable=2086
 
+# action, source, dir, target
+function setup() {
+  case $1 in
+  0)
+    mkdir $3$4
+    ;;
+  1)
+    git clone $2 $3$4
+    ;;
+  2)
+    gh repo clone $2 $3$4
+    ;;
+  esac
+  tmux new-session -d -A -s $4 -c $3$4
+  tmux switch-client -t $4
+  tmux new-window -d -t $4 -c $3$4
+}
+
 dir=$(fd . ~/repos -t d -d 2 | sk)
 if [[ -n $dir ]]; then
   if [[ $(dirname $dir) == $HOME/repos ]]; then
     echo -n "Name: "
     read -r target
     if [[ -n $target ]]; then
-      mkdir -p $dir$target
-      tmux new-session -d -A -s $target -c $dir/$target
-      tmux switch-client -t $target
-      tmux new-window -d -t $target -c $dir/$target
+      source=$target
+      target=$(basename $target .git)
+      if [[ $source =~ ^https?:// || $source =~ ^git@ ]]; then
+        setup 1 $source $dir $target
+      elif [[ $source =~ ^[[:alnum:]-]+/[[:alnum:]-]+ ]]; then
+        setup 2 $source $dir $target
+      else
+        setup 0 "" $dir $target
+      fi
     fi
   else
     target=$(basename $dir)
